ded0b3322b2e55b384cd10f91841eb87be2308e3
Richard Dimalanta <chardmd.au@gmail.com>
2020-11-17 20:12:51 +1100

    removed class

--- components/post/client/about.js
+++ components/post/client/about.js
@@ -1,60 +1,56 @@
 (function() {
-  class About {
-    disabledLoading() {
-      $('#spinner').addClass('d-none');
-      $('#buttonNext').attr('disabled', false);
-      $('#buttonNext .text').text('Capture');
-    }
+  const disabledLoading = () => {
+    $('#spinner').addClass('d-none');
+    $('#buttonNext').attr('disabled', false);
+    $('#buttonNext .text').text('Capture');
+  };
 
-    enableLoading() {
-      $('#spinner').removeClass('d-none');
-      $('#buttonNext').attr('disabled', true);
-      $('#buttonNext .text').text('Saving...');
-    }
+  const enableLoading = () => {
+    $('#spinner').removeClass('d-none');
+    $('#buttonNext').attr('disabled', true);
+    $('#buttonNext .text').text('Saving...');
+  };
 
-    submitForm() {
-      let _this = this;
-      $.ajax({
-        type: 'POST',
-        url: '/post/about',
-        data: $('#aboutForm').serialize(),
-        beforeSend() {
-          _this.enableLoading();
-        },
-        success() {
-          Turbolinks.visit(`/post/preview`);
-        },
-        error(err) {
-          console.log(err);
-          Turbolinks.visit(`/post/about`);
-        }
-      });
-    }
-
-    setButtonActive(text) {
-      let empty = text.length == 0;
-      if (empty) {
-        $('#buttonNext').attr('disabled', true);
-      } else {
-        $('#buttonNext').attr('disabled', false);
+  const submitForm = () => {
+    $.ajax({
+      type: 'POST',
+      url: '/post/about',
+      data: $('#aboutForm').serialize(),
+      beforeSend() {
+        enableLoading();
+      },
+      success() {
+        disabledLoading();
+        Turbolinks.visit(`/post/preview`);
+      },
+      error(err) {
+        console.log(err);
+        Turbolinks.visit(`/post/about`);
       }
+    });
+  };
+
+  const setButtonActive = text => {
+    let empty = text.length == 0;
+    if (empty) {
+      $('#buttonNext').attr('disabled', true);
+    } else {
+      $('#buttonNext').attr('disabled', false);
     }
-  }
+  };
 
   //init and events
   $(function() {
-    const postAbout = new About();
-
     //on input press
     $('#summary').on('keyup', function() {
       let text = $(this).val();
-      postAbout.setButtonActive(text);
+      setButtonActive(text);
     });
 
     //submit form
     $('#aboutForm').on('submit', function(e) {
       e.preventDefault();
-      postAbout.submitForm();
+      submitForm();
     });
   });
 })();
