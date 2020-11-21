c8d78fd8d8274bbe0b2f738355d10c256eeec1a8
Richard Dimalanta <chardmd.au@gmail.com>
2020-11-21 14:29:24 +1100

    removed unused css

--- components/post/client/preview.js
+++ components/post/client/preview.js
@@ -1,55 +1,43 @@
-/* eslint-disable */
+(function() {
+  function disableLoading() {
+    $('#spinner').addClass('d-none');
+    $('#btnPublish').attr('disabled', false);
+    $('#btnPublish .text').text('Publish');
+  }
 
-function disableLoading() {
-  $('#spinner').addClass('d-none');
-  $('#btnPublish').attr('disabled', false);
-  $('#btnPublish .text').text('Publish');
-}
+  function enableLoading() {
+    $('#spinner').removeClass('d-none');
+    $('#btnPublish').attr('disabled', true);
+    $('#btnPublish .text').text('Publishing...');
+  }
 
-function enableLoading() {
-  $('#spinner').removeClass('d-none');
-  $('#btnPublish').attr('disabled', true);
-  $('#btnPublish .text').text('Publishing...');
-}
+  //init and events
+  $(function() {
+    disableLoading();
 
-function initTagify() {
-  // The DOM element you wish to replace with Tagify
-  const input = $('#techStack').get(0);
-  // init Tagify script on the above inputs
-  const tagifyInstance = new Tagify(input);
-  const techStackValue = $('#techStackValue').val();
-  tagifyInstance.addTags(techStackValue);
-}
+    $('#btnPublish').click(function() {
+      const csrf = $('#csrf').val();
+      $.ajax({
+        type: 'PATCH',
+        data: {
+          _csrf: csrf
+        },
+        url: `/post/publish`,
+        beforeSend() {
+          enableLoading();
+        },
+        success() {
+          $('#confirmDialog').modal('hide');
+          Turbolinks.visit(`/post/success`);
+        },
+        error() {
+          Turbolinks.visit(`/post/preview`);
+        }
+      });
+    });
 
-$(function() {
-  // init tagify
-  initTagify();
-
-  // reset button state
-  disableLoading();
-
-  $('#btnConfirm').click(function() {
-    $('#confirmDialog').modal('show');
-  });
-
-  $('#btnPublish').click(function() {
-    const csrf = $('#csrf').val();
-    $.ajax({
-      type: 'PATCH',
-      data: {
-        _csrf: csrf
-      },
-      url: `/post/publish`,
-      beforeSend() {
-        enableLoading();
-      },
-      success() {
-        $('#confirmDialog').modal('hide');
-        Turbolinks.visit(`/post/success`);
-      },
-      error() {
-        Turbolinks.visit(`/post/preview`);
-      }
+    $('#btnConfirm').click(function() {
+      $('#confirmDialog').modal('show');
     });
   });
-});
+})();
