f772793717d65f8071e4edd39efb4cecf3985dad
Richard Dimalanta <chardmd.au@gmail.com>
2020-11-24 18:26:14 +1100

    added tinymce

--- components/app/frontendAssets.js
+++ components/app/frontendAssets.js
@@ -11,7 +11,8 @@ const librariesPath = [
   'node_modules/@yaireo/tagify/dist',
   'node_modules/clipboard/dist',
   'node_modules/html2canvas/dist',
-  'node_modules/@fortawesome/fontawesome-free'
+  'node_modules/@fortawesome/fontawesome-free',
+  'node_modules/tinymce'
 ];
 
 const jsLibraries = librariesPath.map(value =>
