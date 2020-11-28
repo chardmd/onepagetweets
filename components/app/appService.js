86c99fbaf44cd378264b123cdffc36da5f7d3f5b
Richard Dimalanta <chardmd.au@gmail.com>
2020-11-28 12:47:22 +1100

    moved to app component

--- components/app/appService.js
+++ components/app/appService.js
@@ -3,18 +3,18 @@ const exphbs = require('express-handlebars');
 const hbsHelpers = require('handlebars-helpers')();
 
 const hbs = exphbs.create({
-    extname: '.hbs',
-    layoutsDir: 'views/layouts',
-    partialsDir: ['views/partials'],
-    helpers: {
-        ...hbsHelpers,
-        section: function section(name, options) {
-            // setup page specific meta, links, and scripts
-            if (!this._sections) this._sections = {};
-            this._sections[name] = options.fn(this);
-            return null;
-        }
+  extname: '.hbs',
+  layoutsDir: 'components/app/layouts',
+  partialsDir: ['components/app/partials'],
+  helpers: {
+    ...hbsHelpers,
+    section: function section(name, options) {
+      // setup page specific meta, links, and scripts
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
     }
+  }
 });
 
 module.exports = hbs;
