58b9c8795fe1779050089afdaa2b6b403396b636
Richard Dimalanta <chardmd.au@gmail.com>
2020-12-01 18:23:37 +1100

    minor changes

--- components/draft/client/editor.js
+++ components/draft/client/editor.js
@@ -71,11 +71,11 @@
       [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
       [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-      [{ font: [] }],
       [{ align: '' }],
       [{ align: 'center' }],
       [{ align: 'right' }],
-      [{ align: 'justify' }]
+      [{ align: 'justify' }],
+      [{ font: [] }]
     ];
     const editor = new Quill('#editor', {
       modules: {
