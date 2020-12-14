c6f9708b7c05736f05378e985fd3fe2d4ac934b8
Richard Dimalanta <chardmd.au@gmail.com>
2020-12-14 18:45:59 +1100

    updated colors

--- components/draft/draftController.js
+++ components/draft/draftController.js
@@ -12,7 +12,29 @@ exports.getEditor = async (req, res) => {
   res.render('draft/client/editor', {
     title: 'Editor',
     content: project !== null ? project.content : '',
-    bgColor: project !== null ? project.bgColor : ''
+    bgColor: project !== null ? project.bgColor : '',
+    colors: [
+      '#eff6fb',
+      '#ffffff',
+      '#2ecc71',
+      '#3498db',
+      '#9b59b6',
+      '#34495e',
+      '#16a085',
+      '#27ae60',
+      '#2980b9',
+      '#8e44ad',
+      '#2c3e50',
+      '#f1c40f',
+      '#e67e22',
+      '#e74c3c',
+      '#ecf0f1',
+      '#95a5a6',
+      '#f39c12',
+      '#d35400',
+      '#c0392b',
+      '#bdc3c7'
+    ]
   });
 };
 
