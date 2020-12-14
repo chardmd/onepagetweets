b5790a61622aa01c4ee06d4b8ffc883e3db9886f
Richard Dimalanta <chardmd.au@gmail.com>
2020-12-14 18:30:10 +1100

    font sizes

--- components/draft/client/editor.js
+++ components/draft/client/editor.js
@@ -66,18 +66,39 @@
 
   //init and events
   $(() => {
+    const fontSizeArr = [
+      '8px',
+      '9px',
+      '10px',
+      '12px',
+      '14px',
+      '16px',
+      '20px',
+      '24px',
+      '32px',
+      '42px',
+      '54px',
+      '68px',
+      '84px',
+      '98px'
+    ];
     const toolbarOptions = [
-      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+      [{ size: fontSizeArr }],
       [{ align: '' }],
       [{ align: 'center' }],
       [{ align: 'right' }],
       [{ align: 'justify' }],
+      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
       [{ font: [] }]
     ];
+
+    const Size = Quill.import('attributors/style/size');
+    Size.whitelist = fontSizeArr;
+    Quill.register(Size, true);
+
     const editor = new Quill('#editor', {
       modules: {
         toolbar: toolbarOptions
