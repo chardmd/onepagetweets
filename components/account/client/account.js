3cea9aaf0c1f0a83979ba1081e46b090e042533c
Richard Dimalanta <chardmd.au@gmail.com>
2020-12-19 13:30:20 +1100

    ability to cancel your subscription

--- components/account/client/account.js
+++ components/account/client/account.js
@@ -1,4 +1,17 @@
 (() => {
+  const cancelSubscription = () => {
+    const isDeleted = confirm('Do you want to cancel your subscription?');
+    if (isDeleted) {
+      const csrf = $('#csrf').val();
+      $.ajax({
+        type: 'PUT',
+        data: {
+          _csrf: csrf
+        },
+        url: `/account/cancel`
+      });
+    }
+  };
   const deleteAccount = () => {
     const isDeleted = confirm('Do you want to delete this account?');
     if (isDeleted) {
@@ -14,6 +27,9 @@
   };
   //init and events
   $(() => {
+    $('#btnCancelSubscription').on('click', () => {
+      cancelSubscription();
+    });
     $('#btnDeleteAccount').on('click', () => {
       deleteAccount();
     });
